e48b16e1941528680bf63ad4135e5b96668b8cc1
Max Wan <mwan@moneytree.jp>
2023-08-22 18:58:19 +0900

    Fix openService/openServiceUrl option parameter typing
    Previous release introduced a bug where mandatory required attributes for a view options became optional.
    This commit fixes the typing issue.

--- src/api/open-service.ts
+++ src/api/open-service.ts
@@ -1,7 +1,49 @@
 import { getIsTabValue, openWindow } from '../helper';
-import { StoredOptions, ServiceId, OpenServiceOptions } from '../typings';
+import {
+  StoredOptions,
+  ServiceId,
+  OpenServiceOptions,
+  LinkKitOpenServiceOptions,
+  MyAccountOpenServiceOptions,
+  VaultOpenServiceViewServiceList,
+  VaultOpenServiceViewServiceConnection,
+  VaultOpenServiceViewConnectionSetting,
+  VaultOpenServiceViewCustomerSupport,
+  ConfigsOptions
+} from '../typings';
 import openServiceUrl from './open-service-url';
 
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'link-kit',
+  options?: LinkKitOpenServiceOptions
+): void;
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'myaccount',
+  options?: MyAccountOpenServiceOptions
+): void;
+export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: ConfigsOptions): void;
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'vault',
+  options?: VaultOpenServiceViewServiceList
+): void;
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'vault',
+  options?: VaultOpenServiceViewServiceConnection
+): void;
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'vault',
+  options?: VaultOpenServiceViewConnectionSetting
+): void;
+export default function openService(
+  storedOptions: StoredOptions,
+  serviceId: 'vault',
+  options?: VaultOpenServiceViewCustomerSupport
+): void;
 export default function openService(
   storedOptions: StoredOptions,
   serviceId: ServiceId,
@@ -13,5 +55,17 @@ export default function openService(
 
   const { isNewTab, ...restOptions } = options;
 
-  openWindow(openServiceUrl(storedOptions, serviceId, restOptions), getIsTabValue(isNewTab));
+  switch (serviceId) {
+    case 'myaccount':
+      openWindow(openServiceUrl(storedOptions, 'myaccount', restOptions), getIsTabValue(isNewTab));
+      break;
+    case 'vault':
+      openWindow(openServiceUrl(storedOptions, 'vault', restOptions), getIsTabValue(isNewTab));
+      break;
+    case 'link-kit':
+      openWindow(openServiceUrl(storedOptions, 'link-kit', restOptions), getIsTabValue(isNewTab));
+      break;
+    default:
+      throw new Error(`[mt-link-sdk] Invalid \`serviceId\` in \`openService\`, got: ${serviceId}`);
+  }
 }
