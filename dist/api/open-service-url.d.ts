2e417ea3c057abee79269535e24ad08d6dc9ba83
Max Wan <mwan@moneytree.jp>
2023-08-24 10:30:58 +0900

    v3.2.1

--- dist/api/open-service-url.d.ts
+++ dist/api/open-service-url.d.ts
@@ -1,2 +1,8 @@
-import { StoredOptions, ServiceId, OpenServiceUrlOptions } from '../typings';
-export default function openServiceUrl(storedOptions: StoredOptions, serviceId: ServiceId, options?: OpenServiceUrlOptions): string;
+import { StoredOptions, LinkKitOpenServiceUrlOptions, MyAccountOpenServiceUrlOptions, ConfigsOptionsWithoutIsNewTab, VaultOpenServiceUrlViewServiceList, VaultOpenServiceUrlViewServiceConnection, VaultOpenServiceUrlViewConnectionSetting, VaultOpenServiceUrlViewCustomerSupport } from '../typings';
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'link-kit', options?: LinkKitOpenServiceUrlOptions): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'myaccount', options?: MyAccountOpenServiceUrlOptions): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'vault', options?: ConfigsOptionsWithoutIsNewTab): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceUrlViewServiceList): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceUrlViewServiceConnection): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceUrlViewConnectionSetting): string;
+export default function openServiceUrl(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceUrlViewCustomerSupport): string;
