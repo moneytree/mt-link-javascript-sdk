84db1ae4bc4fdf22dfb4557c28006f380ba80c28
Pedro Cuba <pcuba@moneytree.jp>
2024-10-23 12:13:16 +0900

    chore(release): bump to version 4.1.0 :tada:

--- dist/api/open-service.d.ts
+++ dist/api/open-service.d.ts
@@ -1,8 +1,10 @@
-import { StoredOptions, LinkKitOpenServiceOptions, MyAccountOpenServiceOptions, VaultOpenServiceViewServiceList, VaultOpenServiceViewServiceConnection, VaultOpenServiceViewConnectionSetting, VaultOpenServiceViewCustomerSupport, ConfigsOptions } from '../typings';
+import { StoredOptions, LinkKitOpenServiceOptions, MyAccountOpenServiceOptions, VaultOpenServiceViewServiceList, VaultOpenServiceViewServiceConnection, VaultOpenServiceViewConnectionSetting, VaultOpenServiceViewConnectionUpdate, VaultOpenServiceViewConnectionDelete, VaultOpenServiceViewCustomerSupport, ConfigsOptions } from '../typings';
 export default function openService(storedOptions: StoredOptions, serviceId: 'link-kit', options?: LinkKitOpenServiceOptions): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'myaccount', options?: MyAccountOpenServiceOptions): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: ConfigsOptions): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewServiceList): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewServiceConnection): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewConnectionSetting): void;
+export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewConnectionUpdate): void;
+export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewConnectionDelete): void;
 export default function openService(storedOptions: StoredOptions, serviceId: 'vault', options?: VaultOpenServiceViewCustomerSupport): void;
